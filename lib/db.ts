26fd33e7734cf801035ba913b01c6efcd7e037fd
Lucas Siviglia <siviglialucas@gmail.com>
2025-02-18 15:22:08 +1000

    change db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,44 +1,12 @@
 /* eslint-disable no-var */
-// import { MongoClient } from 'mongodb';
-
-// const url = process.env.MONGODB_URL
-// const options = {
-//   useUnifiedTopology: true,
-//   useNewUrlParser: true,
-// }
-
-// let client;
-// let clientPromise;
-
-// if (!process.env.MONGODB_URL) {
-//   throw new Error('Please add your Mongo URI to .env.local')
-// }
-// if (process.env.NODE_ENV === 'development') {
-//   // In development mode, use a global variable so the value is preserved
-//   if (!global._mongoClientPromise) {
-//     client = new MongoClient(url, options)
-//     global._mongoClientPromise = client.connect()
-//   }
-//   clientPromise = global._mongoClientPromise
-// } else {
-//   // In production mode, it's best to not use a global variable
-//   client = new MongoClient(url, options)
-//   clientPromise = client.connect()
-// }
-
-// export default clientPromise;
-
-import { MongoClient, MongoClientOptions } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
 const url = process.env.MONGODB_URL;
-const options: MongoClientOptions = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-};
+// const options: MongoClientOptions = {};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
@@ -50,13 +18,13 @@ if (!process.env.MONGODB_URL) {
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so the value is preserved
   if (!global._mongoClientPromise) {
-    client = new MongoClient(url!, options);
+    client = new MongoClient(url!);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise!;
 } else {
   // In production mode, it's best to not use a global variable
-  client = new MongoClient(url!, options);
+  client = new MongoClient(url!);
   clientPromise = client.connect();
 }
 
